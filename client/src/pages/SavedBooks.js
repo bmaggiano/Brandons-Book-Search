bd7f0d92146c62c739b04e516fd4a2b66fc10c37
bmaggiano <brandon.maggiano@gmail.com>
2022-11-22 22:50:23 -0700

    user books showing up but delete not working

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -9,7 +9,7 @@ import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
   const { loading, data } = useQuery(QUERY_ME)
-  const [removeBook, {error}] = useMutation(REMOVE_BOOK)
+  const [removeBook, { error }] = useMutation(REMOVE_BOOK)
   const userData = data?.me || {}
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
@@ -21,11 +21,9 @@ const SavedBooks = () => {
     }
 
     try {
-      const response = await removeBook(bookId, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      const { data } = await removeBook({
+        variables: { bookId }
+      });
 
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -62,9 +62,10 @@ export const SAVE_BOOK = gql `
     }
 `
 export const REMOVE_BOOK = gql `
-    mutation removeBook($bookId: String) {
+    mutation removeBook($bookId: String!) {
         removeBook(bookId: $bookId) {
             _id
+            email
             username
             email
             savedBooks {
